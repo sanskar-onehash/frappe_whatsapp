cdf159e18659182c169f6ca2d94daa4cd2b3805a
Sanskar Soni <sanskarsoni300@gmail.com>
2025-06-10 16:08:35 +0530

    fix: notification customization from json to code

--- frappe_whatsapp/install.py
+++ frappe_whatsapp/install.py
@@ -5,6 +5,7 @@ from frappe_whatsapp.app_config import APP_TITLE
 def after_install():
     try:
         add_app_to_whatsapp_apps()
+        add_custom_fields_to_notification()
     except Exception as e:
         frappe.log_error(f"Error occured after installing {APP_TITLE} app:", e)
 
@@ -27,3 +28,87 @@ def add_app_to_whatsapp_apps():
             frappe.db.commit()
     except IndexError:
         raise Exception("Notification field custom_whatsapp_app not present.")
+
+
+def add_custom_fields_to_notification():
+    custom_fields = [
+        {
+            "dt": "Notification",
+            "fieldname": "whatsapp_whatsapp_template",
+            "fieldtype": "Link",
+            "label": "Template",
+            "options": "WhatsApp Templates",
+            "insert_after": "custom_whatsapp_app",
+            "module": "Frappe Whatsapp",
+            "depends_on": 'eval: (doc.channel == "WhatsApp") && (doc.custom_whatsapp_app == "WhatsApp")',
+        },
+        {
+            "dt": "Notification",
+            "default": "0",
+            "description": "WhatsApp Template requires a document. ",
+            "fieldname": "whatsapp_attach_document_print",
+            "fieldtype": "Check",
+            "insert_after": "sender_email",
+            "depends_on": 'eval: (doc.channel == "WhatsApp") && (doc.custom_whatsapp_app == "WhatsApp")',
+            "module": "Frappe Whatsapp",
+            "label": " Attach Document Print",
+        },
+        {
+            "dt": "Notification",
+            "default": "0",
+            "description": "WhatsApp Template requires document",
+            "fieldname": "whatsapp_custom_attachment",
+            "fieldtype": "Check",
+            "label": "Custom attachment",
+            "module": "Frappe Whatsapp",
+            "insert_after": "whatsapp_attach_document_print",
+            "depends_on": "eval: (doc.channel == 'WhatsApp') && (doc.custom_whatsapp_app == 'WhatsApp')",
+        },
+        {
+            "dt": "Notification",
+            "depends_on": "eval:doc.whatsapp_custom_attachment",
+            "description": "Upload the file as public. Whatsapp cannot access the private file.",
+            "fieldname": "whatsapp_attach",
+            "fieldtype": "Attach",
+            "module": "Frappe Whatsapp",
+            "insert_after": "whatsapp_custom_attachment",
+            "label": "Attach",
+        },
+        {
+            "dt": "Notification",
+            "depends_on": "eval:doc.whatsapp_custom_attachment",
+            "description": "Attach media from doctype field",
+            "fieldname": "whatsapp_attach_from_field",
+            "fieldtype": "Data",
+            "module": "Frappe Whatsapp",
+            "insert_after": "whatsapp_attach",
+            "label": "Attach from field ",
+        },
+        {
+            "dt": "Notification",
+            "depends_on": "eval:doc.whatsapp_custom_attachment",
+            "fieldname": "whatsapp_file_name",
+            "fieldtype": "Data",
+            "module": "Frappe Whatsapp",
+            "label": "File name",
+            "insert_after": "whatsapp_attach_from_field",
+        },
+        {
+            "dt": "Notification",
+            "description": "Enter fields in the right sequence as per the template parameters.",
+            "fieldname": "whatsapp_fields",
+            "fieldtype": "Table",
+            "label": "Fields",
+            "module": "Frappe Whatsapp",
+            "options": "WhatsApp Message Fields",
+            "insert_after": "condition",
+        },
+    ]
+
+    for field in custom_fields:
+        if not frappe.db.exists(
+            "Custom Field", {"dt": "Notification", "fieldname": field["fieldname"]}
+        ):
+            new_field = frappe.get_doc({"doctype": "Custom Field", **field})
+            new_field.insert()
+            frappe.db.commit()
